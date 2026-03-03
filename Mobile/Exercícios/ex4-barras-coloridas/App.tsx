0ed3224efb69e4ff4540db7692edd96f4be44d21
Eduardo Carmo <98828582+Tidlle@users.noreply.github.com>
2026-03-03 04:09:55 -0300

    Finish ex3

---
+++ Mobile/Exercícios/ex4-barras-coloridas/App.tsx
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <View style={estilos.containerCima}>
+        <View style={[estilos.coresCima, {backgroundColor: 'white'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'yellow'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'cyan'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'green'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'purple'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'red'}]}></View>
+        <View style={[estilos.coresCima, {backgroundColor: 'blue'}]}></View>
+        <StatusBar style="auto" />
+      </View>
+      <View style={estilos.containerBaixo}>
+        <View style={[estilos.coresBaixo, {backgroundColor: 'blue'}]}></View>
+        <View style={[estilos.coresBaixo, {backgroundColor: 'white'}]}></View>
+        <View style={[estilos.coresBaixo, {backgroundColor: 'purple'}]}></View>
+        <View style={[estilos.coresBaixo, {backgroundColor: 'black'}]}></View>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  containerCima: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: "row",
+    margin: 0
+  },
+  coresCima: {
+    width: 50,
+    height: 180,
+  },
+  containerBaixo: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: "row",
+    margin: 0
+  },
+  coresBaixo: {
+    width: 50,
+    height: 40,
+  },
+});
+
