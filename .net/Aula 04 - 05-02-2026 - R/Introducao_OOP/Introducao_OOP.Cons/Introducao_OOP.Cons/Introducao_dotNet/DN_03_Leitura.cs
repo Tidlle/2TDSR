bec247ea167c783c812a836e1b6b4144af011b6a
Eduardo Carmo <98828582+Tidlle@users.noreply.github.com>
2026-03-05 21:33:57 -0300

    add files .net

---
+++ .net/Aula 04 - 05-02-2026 - R/Introducao_OOP/Introducao_OOP.Cons/Introducao_OOP.Cons/Introducao_dotNet/DN_03_Leitura.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Introducao_OOP.Cons.Introducao_dotNet
+{
+    internal class DN_03_Leitura
+    {
+    }
+}
---
+++ .net/Aula 04 - 05-02-2026 - R/Introducao_OOP/Introducao_OOP.Cons/Introducao_OOP.Cons/Introducao_dotNet/DN_04_Constantes.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Introducao_OOP.Cons.Introducao_dotNet
+{
+    internal class DN_04_Constantes
+    {
+        public DN_04_Constantes()
+        {
+            const double PI = 3.1415;
+
+            Console.WriteLine("Valor do PI: {0}", PI);
+        }
+    }
+}
